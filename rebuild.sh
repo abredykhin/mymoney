542a599befb90827c4c696bc64557a972702c8c4
Anton Bredykhin <abredykhin@Antons-MacBook-Air-2.local>
2024-09-22 17:30:38 -0700

    Maybe a checkpoint #3

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-docker-compose down -v --remove-orphans && docker-compose -f docer-compose.yml docker-compose.prod.yml up --build -d
+docker-compose down -v --remove-orphans && docker-compose -f docer-compose.yml -f docker-compose.prod.yml up --build -d
