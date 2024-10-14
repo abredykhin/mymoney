949180d68244524a4181eadd54766c35bcfe788a
Anton Bredykhin <abredykhin@Antons-MacBook-Air-2.local>
2024-10-13 21:29:46 -0700

    Checkpoint: redesign and recent tx

--- server/db/queries/transactions.js
+++ server/db/queries/transactions.js
@@ -66,7 +66,7 @@ const createOrUpdateTransactions = async (userId, transactions) => {
               pending_transaction_transaction_id
             )
           VALUES
-            ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+            ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)
           ON CONFLICT (transaction_id) DO UPDATE 
             SET 
               amount = EXCLUDED.amount,
