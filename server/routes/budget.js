189ea7a02111f91335507f99cb95e764ab5e3dc0
Anton Bredykhin <abredykhin@Antons-MacBook-Air-2.local>
2025-03-02 18:08:54 -0800

    Fix env

--- server/routes/budget.js
+++ server/routes/budget.js
@@ -74,11 +74,11 @@ router.get(
  * -  total of all debits accross all user accounts
  * -  total of all credits accross all user accounts
  * -  last 5 transactions
- * 
+ *
  * @param userId user id in the request body
  * @param forcedRefresh indicates whethere this request is forced refresh. If so, we make a call to Plaid API to refresh user accounts and transactions.
  */
-router.get('summary', verifyToken, asyncWrapper(async (req, res) => {
-  
-});
+// router.get('summary', verifyToken, asyncWrapper(async (req, res) => {
+
+// });
 module.exports = router;
