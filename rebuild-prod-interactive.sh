7b7670bcc711422cae8cb44dc3af46e4876f0764
Anton <abredykhin@gmail.com>
2025-03-13 04:47:51 +0000

    Create scripts folder

--- rebuild-prod-interactive.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-docker-compose down --remove-orphans && docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build
--- run-db-migrations.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-docker-compose exec server knex migrate:latest
