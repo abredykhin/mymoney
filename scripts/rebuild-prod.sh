c722ffc9eafe09216755419d77e38f42fb138591
Anton Bredykhin <abredykhin@Antons-MacBook-Air-2.local>
2025-03-16 22:12:45 -0700

    Optimize Docker

--- scripts/rebuild-prod.sh
+++ scripts/rebuild-prod.sh
@@ -1,3 +1,12 @@
 #!/bin/zsh
 
 docker-compose down -v --remove-orphans && docker-compose -f ../docker-compose.yml -f ../docker-compose.prod.yml up --build -d
+
+# Stop and remove containers but preserve volumes and networks
+docker-compose down --remove-orphans
+
+# Build with cache reuse for faster builds
+docker-compose -f ../docker-compose.yml -f ../docker-compose.prod.yml build --no-cache=false --pull
+
+# Start the containers
+docker-compose -f ../docker-compose.yml -f ../docker-compose.prod.yml up -d
